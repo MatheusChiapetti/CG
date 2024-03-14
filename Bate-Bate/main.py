e84bd04b13a8745dcd583d68825bcb63b57943e4
Matheus Henrique Michels Chiapetti <m.henrique2002@gmail.com>
2024-03-13 21:25:44 -0300

    Aula 13/03/2024

--- Bate-Bate/main.py
+++ Bate-Bate/main.py
@@ -1,7 +1,7 @@
 from Game import Game
 # O main herda as importa as características da classe 'Game', que por sua vez importa da classe 'MovendoTexto'.
 
-# O método abaixo executa o jogo criando um objeto do tipo (Classe) Game.
+# O método abaixo executa o jogo criando um objeto do tipo (classe) Game.
 if __name__ == '__main__':
     game = Game()
     game.run()
