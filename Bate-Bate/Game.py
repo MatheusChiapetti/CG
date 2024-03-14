a81a63eef6057462009dabdb4b63753d50599817
Matheus Henrique Michels Chiapetti <m.henrique2002@gmail.com>
2024-03-13 21:24:07 -0300

    Aula 13/03/2024

--- Bate-Bate/Game.py
+++ Bate-Bate/Game.py
@@ -1,17 +1,20 @@
 import pygame
 import sys
 from MecMovimento import MovendoTexto
+# Acima, a classe 'Game' herda a classe 'MovendoTexto'.
 
 class Game:
+    # A função abaixo é utilizada para construir a classe game. Não há atributos no construtor pois a classe 'Game' herda os atributos da classe 'MovendoTexto'.
     def __init__(self): 
         pygame.init()
-        self.largura = 800
-        self.altura = 600
-        self.tela = pygame.display.set_mode((self.largura, self.altura))
-        pygame.display.set_caption("Bate-Bate")
+        self.largura = 800 # Determina a largua da tela.
+        self.altura = 600  # Determina a altura da tela. 
+        self.tela = pygame.display.set_mode((self.largura, self.altura)) # Função utilizada para criar a tela do jogo.
+        pygame.display.set_caption("Bate-Bate") # Determina o nome da tela.
         self.clock = pygame.time.Clock()
-        self.MovendoTexto = MovendoTexto("DVD", 50, self.largura, self.altura)
+        self.MovendoTexto = MovendoTexto("DVD", 50, self.largura, self.altura) # Cria o objeto, com altura e largura própria, que ficará 'batendo' pela tela. 
         
+    # Função utilizada para rodar o jogo. Ela é disparada ao se criar um objeto do tipo Game no main.
     def run(self):
         rodando = True
         while rodando: 
@@ -20,10 +23,10 @@ class Game:
                     rodando = False
 
             self.MovendoTexto.move()
-            self.tela.fill((0, 0, 0))
-            self.tela.blit(self.MovendoTexto.texto_surf, self.MovendoTexto.rect)
-            pygame.display.flip()
-            self.clock.tick(120)
+            self.tela.fill((0, 0, 0)) # Determina que a cor de fundo da tela.
+            self.tela.blit(self.MovendoTexto.texto_surf, self.MovendoTexto.rect) # A função 'blit' atualiza a tela. 
+            pygame.display.flip() # A funçao 'flip' renderiza a tela. 
+            self.clock.tick(120) # Determina o tempo de tick da tela, que impactará a velocidade do objeto.
         pygame.quit()
         sys.exit()
     
--- Bate-Bate/MecMovimento.py
+++ Bate-Bate/MecMovimento.py
@@ -1,7 +1,10 @@
 import pygame
 import random
 
+# Observa-se que a classe 'MovendoTexto' não importa nenhuma outra classe do projeto.
+
 class MovendoTexto:
+    # Função para construir a classe 'MovendoTexto'.
     def __init__(self, texto, fonte_tamanho, largura, altura):
         self.font = pygame.font.SysFont(None, fonte_tamanho)
         self.texto = texto
@@ -13,44 +16,60 @@ class MovendoTexto:
         self.velocidade_x = self.gerar_numero_nao_zero()
         self.velocidade_y = self.gerar_numero_nao_zero()
         
+    # Função para gerar um número que não seja 0.
     def gerar_numero_nao_zero(self):
         numero = 0
+
+        # Enquanto a variável 'numero' foi igual a 0, o sistema irá sorter um número inteiro entre -1 e 1. O código sairá do loop apenas quando a variável 'numero' for diferente de 0.
         while numero == 0: 
             numero = random.randint(-1, 1)
         return numero
     
     def move(self):
+        # Abaixo, soma-se +1 aos valores das velocidades do objeto nos eixos x e y.
         self.rect.x += self.velocidade_x
         self.rect.y += self.velocidade_y
          
         # Lado Esquerdo:
-        if self.rect.left <= 0:
-            self.velocidade_x = random.randint(0, 1)
+        if self.rect.left <= 0: 
+            # Caso o valor do lado esquerdo seja menor ou igual a 0, o sistema sorteará um novo valor para as velocidades em x e y. 
+            # O valor de x, nesse caso, não pode ser -1. Se for -1, o texto sairá da tela. 
+            self.velocidade_x = random.randint(0, 1) 
             self.velocidade_y = random.randint(-1, 1)
             self.change_color()
             
         # Lado Direito: 
         if self.rect.right >= self.largura:
+            # Caso o valor do lado esquerdo seja menor ou igual a largura da tela, o sistema sorteará um novo valor para as velocidades em x e y. 
+            # O valor de x, nesse caso, não pode ser 1. Se for 1, o texto sairá da tela. 
             self.velocidade_x = random.randint(-1, 0)
             self.velocidade_y = random.randint(-1, 1)
             self.change_color()
             
         # Lado Inferior:
         if self.rect.bottom >= self.altura:
+            # Caso o valor do lado inferior seja menor ou igual a altura da tela, o sistema sorteará um novo valor para as velocidades em x e y. 
+            # O valor de y, nesse caso, não pode ser 1. Se for 1, o texto sairá da tela. 
             self.velocidade_x = random.randint(-1, 1)
             self.velocidade_y = random.randint(-1, 0)
             self.change_color()
             
         # Lado Superior:
         if self.rect.top <= 0: 
+            # Caso o valor do lado superior seja menor ou igual a 0, o sistema sorteará um novo valor para as velocidades em x e y. 
+            # O valor de y, nesse caso, não pode ser -1. Se for -1, o texto sairá da tela. 
             self.velocidade_x = random.randint(-1, 1)
             self.velocidade_y = random.randint(0, 1)
             self.change_color()
             
+    # A função abaixo sorteia uma nova cor para o objeto dentro do expectro RGB toda vez que ele se choca com uma das paredes da tela (Superior, Inferior, Esquerda ou Direita).
     def change_color(self):
+
+        # Os valores são sorteados dentro do intervalo de 0 até 255, e uma nova cor é montada.
         cor_texto = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
                 
         self.texto_surf = self.font.render(self.texto, True, cor_texto)
         
+    # A função 
     def draw(self, tela):
-        tela.blit(self.texto_surf, self.rect)
+        tela.blit(self.texto_surf, self.rect) # # A função 'blit' atualiza (desenha) a variável. 
